5a380e4c5994ed772351c43073a4c98633acfdbd
Hugo Mainland <hugomainland@Hugos-MacBook-Pro.local>
2023-11-28 15:20:25 +1100

    Base Files

---
+++ app.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const path = require("path");
+const methodOverride = require('method-override')
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const ejsMate = require("ejs-mate");
+const Joi = require("joi");
+// this is a model (like a class)
+const Campground = require("./models/campground")
+const ExpressError = require("./utils/ExpressError")
+const catchAsync = require("./utils/catchAsync")
+
+const app = express()
+
+// fire up MONGOOSE
+maingoose().catch(err => console.log(err));
+
+// mongoose main function
+async function maingoose() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+    console.log("Mongoose Connection Open")
+}
+
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride("_method"))
+
+const validateCampground = (req, res, next) => {
+    /// TODO add schema
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(', ');
+        throw new ExpressError(msg, 400)
+    }
+    else {
+        next();
+    }
+}
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine("ejs", ejsMate);
+
+app.listen(3000, () => {
+    console.log("Port open")
+})
+
+app.get("/", (req, res) => {
+    res.render("home");
+})
+
+app.get("/campgrounds", catchAsync(async (req, res, next) => {
+    const campgrounds = await Campground.find({})
+    res.render("campgrounds/index", { campgrounds })
+}))
+
+app.get("/campgrounds/new", (req, res) => {
+    res.render("campgrounds/new")
+})
+
+app.get("/campgrounds/:id", catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render("campgrounds/show", { campground });
+}))
+
+app.post("/campgrounds", validateCampground, catchAsync(async (req, res, next) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground.id}`)
+}))
+
+app.get("/campgrounds/:id/edit", catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render("campgrounds/edit", { campground })
+}))
+
+app.put("/campgrounds/:id", validateCampground, catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${id}`)
+}))
+
+app.delete("/campgrounds/:id", catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndDelete(id);
+    res.redirect("/campgrounds");
+}))
+
+app.all('*', (req, res) => {
+    throw new ExpressError("Page Not Found", 404)
+})
+
+// this is the error handler
+app.use((err, req, res, next) => {
+    const { message = "Error" } = err;
+    if (!err.statusCode) err.statusCode = 500;
+    // res.status(statusCode).send(`<h1>${message}</h1>`)
+    res.status(err.statusCode).render("error", { err })
+})
---
+++ seeds/index.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+const cities = require('./cities');
+const axios = require("axios");
+const Campground = require("../models/campground")
+const { places, descriptors } = require('./seedHelpers');
+
+// fire up MONGOOSE
+maingoose().catch(err => console.log(err));
+
+// mongoose main function
+async function maingoose() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+    console.log("Mongo Connection Open")
+}
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+// call unsplash and return small image
+async function seedImg() {
+    try {
+        const resp = await axios.get('https://api.unsplash.com/photos/random', {
+            params: {
+                client_id: "MjCwvRK2bsm_4VULttuNhqMgw9WVYQuliZF_9XeIkgY",
+                collections: 1114848,
+            },
+        })
+        return resp.data.urls.regular
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+const seedDB = async () => {
+    await Campground.deleteMany({})
+    for (let i = 0; i < 15; i++) {
+        const price = Math.floor(Math.random() * 20) + 10
+
+        // seed data into campground
+        const camp = new Campground({
+            location: `${sample(cities).city}, ${sample(cities).state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            imageUrl: await seedImg(),
+            // imageUrl: "https://images.unsplash.com/photo-1468221296755-1c53a9dbcd54?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            description:
+                'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis, nihil tempora vel aspernatur quod aliquam illum! Iste impedit odio esse neque veniam molestiae eligendi commodi minus, beatae accusantium, doloribus quo!',
+            price
+        })
+
+        await camp.save()
+    }
+}
+
+// seed DB returns a promise because it's an async function
+// this closes the connection after it's done
+seedDB().then(() => {
+    console.log("Camps Seeded, closing Mongo Connection")
+    mongoose.connection.close()
+});
